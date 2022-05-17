afa27b75b96acd4f908717a653eec0bff6dee6ad
Wolker Sanches Dias <wolker.sd@hotmail.com>
2022-05-17 19:44:32 -0400

    back-end
    back-end da aplicação utilizando Node.js e TypeScript.  Persistência das informações no banco de dados com Prisma.

---
+++ server/src/server.ts
@@ -0,0 +1,49 @@
+import express from 'express' 
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma';
+
+const app = express();
+
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "73c1b0ed26a54f",
+      pass: "09c2a9a1a576c9"
+    }
+  });
+
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Wolker Dias <wolkersanches@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    });
+
+    return res.status(201).json({ data: feedback });
+});
+
+app.listen(3333, () => {
+    console.log('HTTP server running!');
+});
+
+// 55:00
